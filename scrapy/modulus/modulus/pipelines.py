2abed8246d5902d582b5098864932e16bbc8b496
Kyle Chu <ktnc.985@gmail.com>
2017-06-28 23:44:37 +0800

    added item pipelines for NUS and NTU

--- scrapy/modulus/modulus/pipelines.py
+++ scrapy/modulus/modulus/pipelines.py
@@ -4,8 +4,40 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class ModulusPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+  def close_spider(self,spider):
+    self.file.close()
+
+  def writeToJson(self,item):
+    output = json.dumps(dict(item)) + '\n'
+    self.file.write(output)
+
+class NusSocPreApprovedPipeline(ModulusPipeline):
+
+  def open_spider(self,spider):
+    self.file = open('NusSocPreApproved.json','w')
+
+  def process_item(self,item,spider):
+    writeToJson(item)
+    return item
+
+class NusModulesPipeline(ModulusPipeline):
+
+  def open_spider(self,spider):
+    self.file = open('NusModules.json','w')
+
+  def process_item(self,item,spider):
+    writeToJson(item)
+    return item
+
+class NtuModulesPipeline(ModulusPipeline):
+
+  def open_spider(self,spider):
+    self.file = open('NtuModules.json','w')
+
+  def process_item(self,item,spider):
+    writeToJson(item)
+    return item
